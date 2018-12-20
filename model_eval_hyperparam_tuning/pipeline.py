a8dce4c81279be25aeceb8ef696a6ea81547e35f
iamdsc <dilpreetchawla382@gmail.com>
2018-12-20 18:51:00 +0530

    Implemented pipelining

---
+++ model_eval_hyperparam_tuning/pipeline.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+
+# Loading the data
+df=pd.read_csv('wdbc.data',header=None)
+X=df.loc[:,2:].values
+y=df.loc[:,1].values
+le=LabelEncoder()
+y=le.fit_transform(y)
+print(np.unique(y))
+
+# Performing train-test split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,stratify=y,random_state=1)
+
+# Making pipeline
+pipe_lr=make_pipeline(StandardScaler(),PCA(n_components=2),LogisticRegression(random_state=1))
+pipe_lr.fit(X_train,y_train)
+y_pred=pipe_lr.predict(X_test)
+print('Test Accuracy: %.3f'%pipe_lr.score(X_test,y_test))
