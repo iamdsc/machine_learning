790fe58fea8dcf4aec053c62971fc7521f319906
iamdsc <dilpreetchawla382@gmail.com>
2019-01-11 00:17:11 +0530

    Plotted decision regions

--- ensemble_learning/majority_vote_classifier.py
+++ ensemble_learning/majority_vote_classifier.py
@@ -12,6 +12,7 @@ from sklearn.metrics import roc_curve, auc
 import numpy as np
 import operator
 import matplotlib.pyplot as plt
+from itertools import product
 
 class MajorityVoteClassifier(BaseEstimator, ClassifierMixin):
     """ A majority vote ensemble classifier """
@@ -91,6 +92,8 @@ for clf, label in zip(all_clf, clf_labels):
     scores=cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
     print('Accuracy: %0.2f (+/- %0.2f)[%s]'%(scores.mean(), scores.std(), label))
 
+# plotting roc-auc
+
 colors=['black', 'orange', 'blue', 'green']
 linestyles=[':', '--', '-.', '-']
 
@@ -108,4 +111,29 @@ plt.ylim([-0.1, 1.1])
 plt.grid(alpha=0.5)
 plt.xlabel('False positive rate (FPR)')
 plt.ylabel('True positive rate (TPR)')
+plt.show()
+
+# plotting the decision region
+
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+x_min = X_train_std[:, 0].min() - 1
+x_max = X_train_std[:, 0].max() + 1
+y_min = X_train_std[:, 1].min() - 1
+y_max = X_train_std[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1), np.arange(y_min, y_max, 0.1))
+f, axarr = plt.subplots(nrows=2, ncols=2, sharex='col', sharey='row', figsize=(7, 5))
+
+for idx, clf, tt in zip(product([0, 1], [0, 1]), all_clf, clf_labels):
+    clf.fit(X_train_std, y_train)
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    axarr[idx[0], idx[1]].contourf(xx, yy, Z, alpha=0.3)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==0, 0], X_train_std[y_train==0, 1], c='blue', marker='^', s=50)
+    axarr[idx[0], idx[1]].scatter(X_train_std[y_train==1, 0], X_train_std[y_train==1, 1], c='green', marker='o', s=50)
+    axarr[idx[0], idx[1]].set_title(tt)
+
+plt.text(-3.5, -4.5, s='Sepal width [standardized]', ha='center', va='center', fontsize=12)
+plt.text(-12.5, 4.5, s='Petal length [standardized]', ha='center', va='center', fontsize=12, rotation=90)
 plt.show()
