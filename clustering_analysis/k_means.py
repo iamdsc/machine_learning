06d4e8be96a64ca07292e43a2cc9039d5826f334
iamdsc <dilpreetchawla382@gmail.com>
2019-01-25 11:54:24 +0530

    Implemented simple k-means clustering

---
+++ clustering_analysis/k_means.py
@@ -0,0 +1,30 @@
+# K-Means Clustering
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_blobs
+from sklearn.cluster import KMeans
+
+# Creating sample data
+X, y=make_blobs(n_samples=150, n_features=2, centers=3, cluster_std=0.5, shuffle=True, random_state=0)
+
+# plot the sample points
+plt.scatter(X[:,0], X[:,1], c='white', marker='o', edgecolor='black', s=50)
+plt.grid()
+plt.show()
+
+# Training the model
+# We set number of desired clusters =3
+# We set n_init=10 to run k-means clustering algo 10 times independently with diff random centroids
+# to choose the final model as the one with lowest SSE
+# via max_iter we specify maximum no. of iterations for each single run
+# tol controls the tolerance with regard to the changes in the within SSE to declare convergence
+km=KMeans(n_clusters=3, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+y_km=km.fit_predict(X)
+
+# plotting the clusters with the centroids
+plt.scatter(X[y_km==0,0], X[y_km==0,1], s=50, c='lightgreen', marker='s', edgecolor='black', label='cluster 1')
+plt.scatter(X[y_km==1,0], X[y_km==1, 1], s=50, c='orange', marker='o', edgecolor='black', label='cluster 2')
+plt.scatter(X[y_km==2,0], X[y_km==2, 1], s=50, c='lightblue', marker='v', edgecolor='black', label='cluster 3')
+plt.scatter(km.cluster_centers_[:,0], km.cluster_centers_[:,1], s=250, marker='*', c='red', edgecolor='black', label='centroids')
+plt.legend(scatterpoints=1)
+plt.grid()
+plt.show()
